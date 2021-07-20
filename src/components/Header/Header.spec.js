be79891919326df6200c433d05d799637bc157e3
logic-fabric <lowic@logic-fabric.fr>
2021-07-20 09:20:42 +0200

    refactor: Header.spec.js

--- src/components/Header/Header.spec.js
+++ src/components/Header/Header.spec.js
@@ -2,9 +2,27 @@ import { render, screen } from "@testing-library/react";
 
 import { Header } from "./Header";
 
-describe("GIVEN a connected user on a page with a Header", () => {
+describe("GIVEN a user on a page with a Header", () => {
   beforeEach(() => {
     render(<Header />);
+  });
+
+  test("THEN the Header contains a logo with an alt text", () => {
+    const logo = screen.getByAltText(/SportSee/i);
+    
+    expect(logo).toBeTruthy();
+  });
+
+  test("THEN there is a navigation with a list and 4 listitems for the main links", () => {
+    const navElement = screen.getByRole("navigation");
+    expect(navElement).toBeTruthy();
+
+    const listElement = screen.getByRole("list");
+    expect(listElement).toBeTruthy();
+
+    const listItemElements = screen.getAllByRole("listitem");
+    expect(listItemElements).toBeTruthy();
+    expect(listItemElements.length).toBe(4);
   });
 
   test("THEN the Header contains 5 links", () => {
@@ -13,31 +31,17 @@ describe("GIVEN a connected user on a page with a Header", () => {
     expect(links.length).toBe(5);
   });
 
-  test("THEN The Header contains an accessible logo", () => {
-    const logo = screen.getAllByAltText(/SportSee/i);
-
-    expect(logo).toBeTruthy();
-  });
-
   test("THEN there are links for Home, Profile, Settings and Community", () => {
     const homeLink = screen.getByText(/Accueil/);
-    const profileLink = screen.getByText(/Profil/);
-    const settingsLink = screen.getByText(/Réglages/);
-    const communityLink = screen.getByText(/Communauté/);
-
     expect(homeLink).toBeTruthy();
+
+    const profileLink = screen.getByText(/Profil/);
     expect(profileLink).toBeTruthy();
+
+    const settingsLink = screen.getByText(/Réglages/);
     expect(settingsLink).toBeTruthy();
+
+    const communityLink = screen.getByText(/Communauté/);
     expect(communityLink).toBeTruthy();
-  });
-
-  test("THEN there is a <nav> containing a <ul> and some <li> for the main links", () => {
-    const navElement = screen.getByRole("navigation");
-    const listElement = screen.getByRole("list");
-    const listItemElement = screen.getAllByRole("listitem");
-
-    expect(navElement).toBeTruthy();
-    expect(listElement).toBeTruthy();
-    expect(listItemElement).toBeTruthy();
   });
 });
