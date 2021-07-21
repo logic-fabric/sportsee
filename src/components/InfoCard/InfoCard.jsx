2787730620d3aeb3da1a2b2d0d70f23d6605dbcf
logic-fabric <lowic@logic-fabric.fr>
2021-07-21 14:37:57 +0200

    feature: build InfoCard HTML (tdd)

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,8 +1,32 @@
+import { toFrenchIntegerFormat } from "../../utils/format";
+import calorieIcon from "../../assets/icon-calorie.png";
+import carbohydrateIcon from "../../assets/icon-carbohydrate.png";
+import lipidIcon from "../../assets/icon-lipid.png";
+import proteinIcon from "../../assets/icon-protein.png";
+
+const ICON_BY_TYPE = {
+  Calories: calorieIcon,
+  Glucides: carbohydrateIcon,
+  Protéines: proteinIcon,
+  Lipides: lipidIcon,
+};
+
+const UNIT_BY_TYPE = {
+  Calories: "kCal",
+  Glucides: "g",
+  Protéines: "g",
+  Lipides: "g",
+};
+
 export function InfoCard({ type, value }) {
   return (
     <div>
-      <p>{type}</p>
-      <p>{value}</p>
+      <img src={ICON_BY_TYPE[type]} alt="" />
+      <p data-testid="card-measure">
+        {toFrenchIntegerFormat(value)}
+        {UNIT_BY_TYPE[type]}
+      </p>
+      <p data-testid="card-type">{type}</p>
     </div>
   );
 }
---
+++ src/components/InfoCard/InfoCard.spec.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+
+import { InfoCard } from "./InfoCard";
+
+
+describe("GIVEN a user on a page with a CardInfo of type 'Calories'", () => {
+  test("THEN 'Calories' and the value with the right unit is displayed", () => {
+    render(<InfoCard type="Calories" value="4242" />);
+
+    const cardType = screen.getByTestId("card-type");
+    const cardMeasure = screen.getByTestId("card-measure");
+
+    expect(cardType.textContent).toBe("Calories");
+    expect(cardMeasure.textContent).toBe("4.242kCal");
+  });
+});
+
+describe("GIVEN a user on a page with a CardInfo of type 'Protéines'", () => {
+  test("THEN 'Protéines' and the value with the right unit is displayed", () => {
+    render(<InfoCard type="Protéines" value="4242" />);
+
+    const cardType = screen.getByTestId("card-type");
+    const cardMeasure = screen.getByTestId("card-measure");
+
+    expect(cardType.textContent).toBe("Protéines");
+    expect(cardMeasure.textContent).toBe("4.242g");
+  });
+});
