03d33ee7ece220802b6bafd4cad935579ac45fa3
logic-fabric <lowic@logic-fabric.fr>
2021-08-14 08:59:51 +0200

    data: remove 'Mocked' from fake users names

--- src/data/mockedData.js
+++ src/data/mockedData.js
@@ -2,7 +2,7 @@ export const USER_MAIN_DATA = [
   {
     id: 12,
     userInfos: {
-      firstName: "MockedKarl",
+      firstName: "Karl",
       lastName: "Dovineau",
       age: 31,
     },
@@ -17,7 +17,7 @@ export const USER_MAIN_DATA = [
   {
     id: 18,
     userInfos: {
-      firstName: "MockedCecilia",
+      firstName: "Cecilia",
       lastName: "Ratorez",
       age: 34,
     },
